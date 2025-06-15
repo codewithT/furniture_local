8fe9fe2be6cd4ea1826304ffa4f8c0f9c7ddfd62
M Tulasi Ram <108621191+codewithT@users.noreply.github.com>
2025-06-15 12:04:03 +0530

    Update environment.prod.ts

--- furniture-app/src/environments/environment.prod.ts
+++ furniture-app/src/environments/environment.prod.ts
@@ -1,10 +1,6 @@
-// export const environment = {
-//     production: true,
-//     apiBaseUrl: 'https://your-production-api.com/api' // Production API URL
-//   };
-  
+ 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://sdmssschool.com/furniture2', // Replace with your actual API base URL
-  BASE_URL : '/furniture'
+  apiBaseUrl: '', // Replace with your actual API base URL
+  
 };
